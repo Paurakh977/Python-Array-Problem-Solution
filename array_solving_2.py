2f4fdee682c0adfc5bf45c87506ae807a2d2a61c
“Paurakh977” <pandeypaurakhraj@gmail.com>
2023-09-11 18:34:12 +0545

    Sorting of Array in Specific Pattern

---
+++ array_solving_2.py
@@ -0,0 +1,25 @@
+# You are give a list [a,b,c,d,e,f]
+# you have to sort in such a way that 1st last last term of the list come alternatively
+# ie: [a,f,b,e,c,d]
+
+def give_me_your_list(list_):
+    print(f"Your given list {list_}")
+    b=[]
+    def checker(x):
+        return len(x)==len(list_)
+    
+    j=len(list_)
+    for i in range(len(list_)):
+        b.append(list_[i])
+        if checker(b):
+            break
+        b.append(list_[j-1])
+        if checker(b):
+            break
+        j-=1
+    return f"your sorted list{b}"
+
+print(give_me_your_list([1,2,3,4,5]))
+
+
+
